0838a578fe94382419d775dd0942dfbc2be7c219
JSRGolu <l.vashisht@se24.qmul.ac.uk>
2025-04-14 03:45:02 +0100

    Organize project folder

--- Generator/Assets/Scripts/MapGenerator.cs
+++ Generator/Assets/Scripts/MapGenerator.cs
@@ -1,8 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 /*
  * Based on "Procedural Cave Generation" by Sebastian Lague
@@ -11,12 +10,13 @@ using static UnityEditor.PlayerSettings;
 public class MapGenerator : MonoBehaviour
 {
     // Serialized Variables
-	[Header("Map Dimensions")]
+    [Header("Map Dimensions")]
     public int width;
-	public int height;
+    public int height;
 
-	[Header("Prefabs")]
-	public GameObject pointPrefab;
+    [Header("CheckPoints")]
+    public GameObject pointPrefab;
+    public int startPointOffset;
 
     [Header("Seed Control")]
     public string seed;
@@ -25,16 +25,16 @@ public class MapGenerator : MonoBehaviour
     [Header("Trees Generation")]
     [Range(0, 100)]
     public int randomFillPercent;
-	public int numPatches;
-	public int patchRadius;
+    public int numPatches;
+    public int patchRadius;
     public int smoothning;
 
     [Header("Pathing")]
     public float maxDeviation;
     public int segments;
 
-    [Header("Visualiser")]
-    public bool useMesh;
+    [Header("Generation Delay")]
+    public float stepDelay;
 
     // Private Variables
     private int[,] map;
@@ -44,74 +44,65 @@ public class MapGenerator : MonoBehaviour
     private List<GameObject> spawnedObjects = new List<GameObject>();
     private List<Vector2Int> patchCenters = new List<Vector2Int>();
     private List<Vector2Int> path = new List<Vector2Int>();
-	private List<Vector2Int> waypoints = new List<Vector2Int>();
+    private List<Vector2Int> waypoints = new List<Vector2Int>();
     private List<Vector2> pathPoints = new List<Vector2>();
+    private bool isGenerating = false;
 
     /*
 	 * Generate the map on start, on mouse click
 	 */
     void Start()
-	{
-		GenerateMap();
-	}
+    {
+        StartCoroutine(GenerateMap());
+    }
 
-	void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			GenerateMap();
-		}
-	}
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isGenerating)
+        {
+            StartCoroutine(GenerateMap());
+        }
+    }
 
-	void GenerateMap()
-	{
-		map = new int[width, height];
+    IEnumerator GenerateMap()
+    {
+        isGenerating = true;
+        Debug.Log("Generating Map: " + isGenerating);
+
+        map = new int[width, height];
 
         path.Clear();
         patchCenters.Clear();
         waypoints.Clear();
         pathPoints.Clear();
-
+        ClearSpawnObjects();
+        
         StartRandomSeed();
 
         // Stage 1: populate the grid cells
-        PopulateMap();
+        yield return StartCoroutine(PopulateMap());
 
-		// Stage 2: apply cellular automata rules
-		for (int i = 0; i < smoothning; i++)
-		{
-			SmoothMap();
-		}
-
-		// Stage 3: finalise the map
-		ProcessMap();
-		AddMapBorder();
-
-		// Generate mesh
-		if(useMesh)
-		{
-            MeshGenerator meshGen = GetComponent<MeshGenerator>();
-            meshGen.GenerateMesh(map, 1);
+        // Stage 2: apply cellular automata rules
+        for (int i = 0; i < smoothning; i++)
+        {
+            yield return StartCoroutine(SmoothMap());
+            GenerateMesh();
+            yield return new WaitForSeconds(stepDelay);
         }
-		else
-		{
-            MapVisualiser mapViz = GetComponent<MapVisualiser>();
-            mapViz.VisualizeGrid(map, 1);
-        }
-	}
 
-	/*
-	 * STAGE 1: Populate the map
-	 */
-	void PopulateMap()
-	{
-        ForestFill();
-        StartAndEntPoint();
-        CreatePatches();
+        // Stage 3: finalise the map
+        yield return StartCoroutine(ProcessMap());
+        yield return StartCoroutine(AddMapBorder());
+
+        // Generate mesh
+        GenerateMesh();
+
+        isGenerating = false;
+        Debug.Log("Generating Map: " + isGenerating);
     }
 
-	void StartRandomSeed()
-	{
+    void StartRandomSeed()
+    {
         if (useRandomSeed)
         {
             seed = Time.time.ToString();
@@ -120,40 +111,87 @@ public class MapGenerator : MonoBehaviour
         pseudoRandom = new System.Random(seed.GetHashCode());
     }
 
-	void StartAndEntPoint()
-	{
-		ClearSpawnObjects();
-
-		startPoint = new Vector2Int(2, 2);
-        endPoint = new Vector2Int(pseudoRandom.Next(width/2, width - 2), pseudoRandom.Next(height/2, height - 2));
-
-		PlaceGameObject(pointPrefab, startPoint, Color.black);
-		PlaceGameObject(pointPrefab, endPoint, Color.red);
+    void GenerateMesh()
+    {
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        if (meshGen != null)
+            meshGen.GenerateMesh(map, 1);
     }
 
-	void PlaceGameObject(GameObject prefab, Vector2Int point, Color color)
-	{
+    /*
+	 * STAGE 1: Populate the map
+	 */
+    IEnumerator PopulateMap()
+    {
+        yield return StartCoroutine(ForestFill());
+        GenerateMesh();
+        yield return new WaitForSeconds(stepDelay);
+
+        yield return StartCoroutine(StartAndEndPoint());
+        GenerateMesh();
+        yield return new WaitForSeconds(stepDelay);
+
+        yield return StartCoroutine(CreatePatches());
+        GenerateMesh();
+        yield return new WaitForSeconds(stepDelay);
+    }
+
+    IEnumerator StartAndEndPoint()
+    {
+        int edge = pseudoRandom.Next(0, 4);
+
+        switch (edge)
+        {
+            case 0: // Left edge
+                startPoint = new Vector2Int(startPointOffset, pseudoRandom.Next(startPointOffset, height - startPointOffset));
+                break;
+            case 1: // Right edge
+                startPoint = new Vector2Int(width - 1 - startPointOffset, pseudoRandom.Next(startPointOffset, height - startPointOffset));
+                break;
+            case 2: // Top edge
+                startPoint = new Vector2Int(pseudoRandom.Next(startPointOffset, width - startPointOffset), height - 1 - startPointOffset);
+                break;
+            case 3: // Bottom edge
+                startPoint = new Vector2Int(pseudoRandom.Next(startPointOffset, width - startPointOffset), startPointOffset);
+                break;
+        }
+
+        bool isLeft = startPoint.x < width / 2;
+        bool isBottom = startPoint.y < height / 2;
+
+        if (isLeft && isBottom)
+            endPoint = new Vector2Int(pseudoRandom.Next(width / 2, width - 2), pseudoRandom.Next(height / 2, height - 2));
+
+        else if (!isLeft && !isBottom)
+            endPoint = new Vector2Int(pseudoRandom.Next(2, width / 2), pseudoRandom.Next(2, height / 2));
+
+        else if (isLeft && !isBottom)
+            endPoint = new Vector2Int(pseudoRandom.Next(width / 2, width - 2), pseudoRandom.Next(2, height / 2));
+
+        else if (!isLeft && isBottom)
+            endPoint = new Vector2Int(pseudoRandom.Next(2, width / 2), pseudoRandom.Next(height / 2, height - 2));
+
+        PlaceGameObject(pointPrefab, startPoint, Color.black);
+        yield return new WaitForSeconds(stepDelay / 2);
+        PlaceGameObject(pointPrefab, endPoint, Color.red);
+    }
+
+    void PlaceGameObject(GameObject prefab, Vector2Int point, Color color)
+    {
         Vector3 pos = new Vector3((point.x - width / 2), 0f, (point.y - height / 2));
 
-        GameObject newObj = Instantiate(prefab, pos, Quaternion.Euler(90f,0f,0f));
+        GameObject newObj = Instantiate(prefab, pos, Quaternion.Euler(90f, 0f, 0f));
         spawnedObjects.Add(newObj);
 
         Renderer renderer = newObj.GetComponent<Renderer>();
-		if (renderer != null)
-			renderer.material.color = color;
+        if (renderer != null)
+            renderer.material.color = color;
 
-		ClearForest(point, 3);
+        ClearForest(point, 3);
     }
 
-    void ForestFill()
+    IEnumerator ForestFill()
     {
-        if (useRandomSeed)
-        {
-            seed = Time.time.ToString();
-        }
-
-        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
-
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -168,17 +206,18 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
+        yield return null;
     }
 
-	void CreatePatches()
-	{
+    IEnumerator CreatePatches()
+    {
         for (int i = 0; i < numPatches; i++)
-		{
+        {
             int x = pseudoRandom.Next(2, width - 2);
             int y = pseudoRandom.Next(2, height - 2);
 
             Vector2Int center = new Vector2Int(x, y);
-
+            
             if (Vector2Int.Distance(center, startPoint) < patchRadius + 2 || Vector2Int.Distance(center, endPoint) < patchRadius + 2)
             {
                 i--;
@@ -187,13 +226,16 @@ public class MapGenerator : MonoBehaviour
 
             patchCenters.Add(center);
 
-			ClearForest(center, patchRadius);
+            ClearForest(center, patchRadius);
+
+            GenerateMesh();
+            yield return new WaitForSeconds(stepDelay / 2);
         }
         path.AddRange(patchCenters);
     }
 
-	void ClearForest(Vector2Int pCenter, int pRadius)
-	{
+    void ClearForest(Vector2Int pCenter, int pRadius)
+    {
         for (int patchX = -pRadius; patchX <= pRadius; patchX++)
         {
             for (int patchY = -pRadius; patchY <= pRadius; patchY++)
@@ -212,8 +254,8 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-	void ClearSpawnObjects()
-	{
+    void ClearSpawnObjects()
+    {
         foreach (GameObject obj in spawnedObjects)
         {
             if (obj != null)
@@ -224,65 +266,72 @@ public class MapGenerator : MonoBehaviour
         spawnedObjects.Clear();
     }
 
-	/*
+    /*
 	 * STAGE 2: Smooth map with CA
 	 */
-	void SmoothMap()
-	{
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				int neighbourWallTiles = GetSurroundingWallCount(x, y);
+    IEnumerator SmoothMap()
+    {
+        int[,] newMap = new int[width, height];
 
-				if (neighbourWallTiles > 4)
-					map[x, y] = 1;
-				else if (neighbourWallTiles < 3)
-					map[x, y] = 0;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
 
-			}
-		}
-	}
+                if (neighbourWallTiles > 4)
+                    newMap[x, y] = 1;
+                else if (neighbourWallTiles < 3)
+                    newMap[x, y] = 0;
+                else
+                    newMap[x, y] = map[x, y];
+            }
+        }
 
-	int GetSurroundingWallCount(int gridX, int gridY)
-	{
-		int wallCount = 0;
-		for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
-		{
-			for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
-			{
-				if (IsInMapRange(neighbourX, neighbourY))
-				{
-					if (neighbourX != gridX || neighbourY != gridY)
-					{
-						wallCount += map[neighbourX, neighbourY];
-					}
-				}
-				else
-				{
-					wallCount++;
-				}
-			}
-		}
+        map = newMap;
+        yield return null;
 
-		return wallCount;
-	}
-
-	bool IsInMapRange(int x, int y)
-	{
-		return (x >= 0 && x < width && y >= 0 && y < height);
-	}
-
-
-	/*
-	 * Stage 3: produce the finished map
-	 */
-	void ProcessMap()
-	{
-        GeneratePathing();
     }
 
-    void GeneratePathing()
+    int GetSurroundingWallCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (IsInMapRange(neighbourX, neighbourY))
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+
+        return wallCount;
+    }
+
+    bool IsInMapRange(int x, int y)
+    {
+        return (x >= 0 && x < width && y >= 0 && y < height);
+    }
+
+
+    /*
+	 * Stage 3: produce the finished map
+	 */
+    IEnumerator ProcessMap()
+    {
+        yield return StartCoroutine(GeneratePathing());
+    }
+
+    IEnumerator GeneratePathing()
     {
         waypoints.AddRange(path);
         waypoints.Add(endPoint);
@@ -304,7 +353,9 @@ public class MapGenerator : MonoBehaviour
                 }
             }
 
-            DrawPath(currentPoint, nearest);
+            yield return StartCoroutine(DrawPath(currentPoint, nearest));
+            GenerateMesh();
+            yield return new WaitForSeconds(stepDelay);
 
             currentPoint = nearest;
             visited.Add(currentPoint);
@@ -312,7 +363,7 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-    void DrawPath(Vector2Int from, Vector2Int to)
+    IEnumerator DrawPath(Vector2Int from, Vector2Int to)
     {
         GenerateCurvedPath(from, to, segments, maxDeviation, pathPoints);
 
@@ -348,6 +399,7 @@ public class MapGenerator : MonoBehaviour
                 }
             }
         }
+        yield return null;
     }
 
     void GenerateCurvedPath(Vector2 from, Vector2 to, int segments, float maxDeviation, List<Vector2> pathPoints)
@@ -376,25 +428,26 @@ public class MapGenerator : MonoBehaviour
         GenerateCurvedPath(midpoint, to, segments - 1, maxDeviation * 0.5f, pathPoints);
     }
 
-    void AddMapBorder()
-	{
-		int borderSize = 1;
-		int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+    IEnumerator AddMapBorder()
+    {
+        int borderSize = 1;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
 
-		for (int x = 0; x < borderedMap.GetLength(0); x++)
-		{
-			for (int y = 0; y < borderedMap.GetLength(1); y++)
-			{
-				if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
-				{
-					borderedMap[x, y] = map[x - borderSize, y - borderSize];
-				}
-				else
-				{
-					borderedMap[x, y] = 1;
-				}
-			}
-		}
-		map = borderedMap;
-	}
+        for (int x = 0; x < borderedMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); y++)
+            {
+                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+                {
+                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+                }
+                else
+                {
+                    borderedMap[x, y] = 1;
+                }
+            }
+        }
+        map = borderedMap;
+        yield return null;
+    }
 }
